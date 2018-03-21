b913599ac051626d58c64f2f697a5dd33a650923
Elijah Wilson <elijah.b.wilson@gmail.com>
2018-03-21 16:31:34 -0600

    finished table build and faker data for payment types

--- build-db.js
+++ build-db.js
@@ -5,6 +5,7 @@ const { buildComputerTable } = require('./databases/computers');
 const { createCustomerDatabase } = require('./databases/customers');
 const { buildProgramsTable } = require('./databases/training-programs');
 const { buildProductTypesTable } = require('./databases/prod-types');
+const { buildPaymentTypesTable } = require('./databases/payment-types');
 
 
 setTimeout(buildProgramsTable, 0);
@@ -12,3 +13,5 @@ setTimeout(buildDepartmentTable, 50);
 setTimeout(createCustomerDatabase, 100);
 setTimeout(buildComputerTable, 150);
 setTimeout(buildProductTypesTable, 200);
+setTimeout(buildPaymentTypesTable, 350);
+

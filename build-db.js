cb5ec37b816055bc814da41292034fcc6cd26f36
Elijah Wilson <elijah.b.wilson@gmail.com>
2018-03-21 14:02:40 -0600

    resolved final conflict

--- build-db.js
+++ build-db.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-const { buildComputerTable } = require('./databases/computers')
-
-buildComputerTable();
-=======
 
 const { buildDepartmentTable } = require('./databases/departments');
 const { buildComputerTable } = require('./databases/computers');
@@ -11,4 +6,3 @@ const { createCustomerDatabase } = require('./databases/customers');
 setTimeout(buildDepartmentTable, 0);
 setTimeout(createCustomerDatabase, 50);
 setTimeout(buildComputerTable, 100);
->>>>>>> master

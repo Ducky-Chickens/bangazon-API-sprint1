e6bfcb5a49ba558f579d96ca63f43472b94f239c
Elijah Wilson <elijah.b.wilson@gmail.com>
2018-03-21 13:54:27 -0600

    template for all future project folders

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes/computers');
+const routes = require('./routes/computers-route');
 
 app.use("/api/", routes);
 

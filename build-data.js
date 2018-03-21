20b65c7b02ba9d992dcc5d175df8ba434312d8ca
Elijah Wilson <elijah.b.wilson@gmail.com>
2018-03-21 11:10:16 -0600

    computers faker data generator and database builder features finished

---
+++ build-data.js
@@ -0,0 +1,6 @@
+const { createWriteStream } = require('fs');
+const { generateComputers } = require('./generators/computer-faker');
+
+let computers = generateComputers();
+let compStream = createWriteStream(`./data/computers.json`);
+compStream.write(JSON.stringify(computers));
---
+++ databases/computers.js
@@ -0,0 +1,20 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('bangazon.sqlite');
+const { readFileSync } = require('fs');
+const { computers } = JSON.parse(readFileSync("./data/computers.json"));
+
+module.exports.buildComputerTable = () => 
+  db.serialize(() => {
+    // db.run(`DROP TABLE IF EXISTS computers`);
+    db.run(
+      `CREATE TABLE IF NOT EXISTS computers (
+        computer_id INTEGER PRIMARY KEY,
+        purchase_date TEXT,
+        decomission_date TEXT
+      )`,
+      () => {
+        computers.forEach(({ purchaseDate }) => {
+          db.run(`INSERT INTO computers VALUES (${null}, "${purchaseDate}", ${null})`);
+      });
+    })
+  });

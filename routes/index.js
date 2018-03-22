63d70738bd1008ad2778494a70273b9a19617c26
Hunter Phillips <hkphillips42@gmail.com>
2018-03-22 15:10:08 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 
-<<<<<<< HEAD
 router.use(require('./computers-route'))
 router.use(require('./departments-route'))
-=======
-router.use(require('./computers-route'));
 router.use(require('./programs-route'));
->>>>>>> master
+
 
 module.exports = router;

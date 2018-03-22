213d7f6e62ea89fc1bf2d94292493a010e1cef0a
Eli Wilson <31283824+e-x-wilson@users.noreply.github.com>
2018-03-22 08:24:36 -0600

    Update employees.js

--- databases/employees.js
+++ databases/employees.js
@@ -5,33 +5,27 @@ const { employees } = JSON.parse(readFileSync('./data/employees.json'));
 
 module.exports.buildEmployeeTable = () => {
 
-	db.serialize(function () {
-		db.run(`DROP TABLE IF EXISTS employees`);
-		db.run(
-			`CREATE TABLE IF NOT EXISTS employees (
-			employee_id INTEGER PRIMARY KEY,
+  db.serialize(function () {
+    db.run(`DROP TABLE IF EXISTS employees`);
+    db.run(
+      `CREATE TABLE IF NOT EXISTS employees (
+      employee_id INTEGER PRIMARY KEY,
       first_name TEXT,
       last_name TEXT,
-			email TEXT,
-			department_id INTEGER,
+      email TEXT,
+      department_id INTEGER,
       FOREIGN KEY (department_id) REFERENCES departments (department_id)
-		)`,
-			() => {
-				employees.forEach(({
-					first_name,
-					last_name,
-          email,
-          department_id
-				}) => {
-					db.run(`INSERT INTO employees VALUES (
-					${null},
-					"${first_name}",
-					"${last_name}",
+      )`,
+      () => {employees.forEach(({first_name, last_name, email, department_id}) => {
+        db.run(`INSERT INTO employees VALUES (
+	  ${null},
+	  "${first_name}",
+	  "${last_name}",
           "${email}",
           ${department_id}
-				)`);
-				});
-			}
-		)
-	});
+	)`);
+       });
+      }
+    )
+  });
 };

0f7d2d587d6550a1cb20e4bc6ec3c21d453805d0
dhazlettjr <d.hazlett.jr@gmail.com>
2018-03-22 08:08:10 -0500

    added employee build table

--- build-db.js
+++ build-db.js
@@ -4,8 +4,10 @@ const { buildDepartmentTable } = require('./databases/departments');
 const { buildComputerTable } = require('./databases/computers');
 const { createCustomerDatabase } = require('./databases/customers');
 const { buildProgramsTable } = require('./databases/training-programs');
+const { buildEmployeeTable } = require('./databases/employees');
 
 setTimeout(buildProgramsTable, 0);
 setTimeout(buildDepartmentTable, 50);
 setTimeout(createCustomerDatabase, 100);
 setTimeout(buildComputerTable, 150);
+setTimeout(buildEmployeeTable, 200);

4fa04077a07d864b066c69d0b131b26e7b5b990d
Eli Wilson <31283824+e-x-wilson@users.noreply.github.com>
2018-03-22 08:30:54 -0600

    Update departments.js

--- databases/departments.js
+++ databases/departments.js
@@ -5,29 +5,23 @@ const {departments} = JSON.parse(readFileSync('./data/departments.json'));
 
 module.exports.buildDepartmentTable = () => {
 
-	db.serialize(function () {
-		db.run(`DROP TABLE IF EXISTS departments`);
-		db.run(
-			`CREATE TABLE IF NOT EXISTS departments (
-			department_id INTEGER PRIMARY KEY,
-			dept_name TEXT,
-			supervisor_id INTEGER,
-			budget INTEGER 
-		)`,
-			() => {
-				departments.forEach(({
-					dept_name,
-					supervisor_id,
-					budget
-				}) => {
-					db.run(`INSERT INTO departments VALUES (
-					${null},
-					"${dept_name}",
-					"${supervisor_id}",
-					${budget}
-				)`);
-				});
-			}
-		)
-	});
+  db.serialize(function () {
+  db.run(`DROP TABLE IF EXISTS departments`);
+  db.run(
+    `CREATE TABLE IF NOT EXISTS departments (
+    department_id INTEGER PRIMARY KEY,
+    dept_name TEXT,
+    supervisor_id INTEGER,
+    budget INTEGER 
+    )`, () => {
+      departments.forEach(({ dept_name, supervisor_id, budget}) => {
+	db.run(`INSERT INTO departments VALUES (
+	  ${null},
+	  "${dept_name}",
+	  "${supervisor_id}",
+	  ${budget}
+        )`);
+      });
+    })
+  });
 };

e08bcffb8f08f9480aac13bdfe55460d3b54343f
Valerie Freeman <valerah7@gmail.com>
2018-03-22 13:59:21 -0500

    Create table for order_product

--- build-db.js
+++ build-db.js
@@ -1,4 +1,4 @@
-'use strict'; 
+'use strict';
 
 const { buildDepartmentTable } = require('./databases/departments');
 const { buildComputerTable } = require('./databases/computers');
@@ -9,16 +9,16 @@ const { buildProductTypesTable } = require('./databases/prod-types');
 const { buildProductTable } = require('./databases/products')
 const { buildPaymentTypesTable } = require('./databases/payment-types');
 const { buildOrderTable } = require('./databases/orders');
-
+const { buildOrderProductTable } = require('./databases/order_product');
 
 
 setTimeout(buildProgramsTable, 0);
-setTimeout(buildDepartmentTable, 50);
-setTimeout(buildCustomerTable, 100);
-setTimeout(buildComputerTable, 150);
-setTimeout(buildProductTypesTable, 200);
-setTimeout(buildProductTable, 250);
-setTimeout(buildEmployeeTable, 350);
-setTimeout(buildPaymentTypesTable, 400);
-setTimeout(buildOrderTable, 450);
-
+setTimeout(buildDepartmentTable, 100);
+setTimeout(buildCustomerTable, 200);
+setTimeout(buildComputerTable, 300);
+setTimeout(buildProductTypesTable, 400);
+setTimeout(buildProductTable, 500);
+setTimeout(buildEmployeeTable, 600);
+setTimeout(buildPaymentTypesTable, 700);
+setTimeout(buildOrderTable, 800);
+setTimeout(buildOrderProductTable, 900);

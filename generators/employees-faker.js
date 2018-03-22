41b716af7254d7a8c4776ec5ef93a5b774e9a8f2
Eli Wilson <31283824+e-x-wilson@users.noreply.github.com>
2018-03-22 08:28:14 -0600

    Update employees-faker.js

--- generators/employees-faker.js
+++ generators/employees-faker.js
@@ -7,26 +7,25 @@ const { createWriteStream } = require('fs');
 
 
 let generateEmployees = () => {
-	let employees = [];
+  let employees = [];
 	
-	for (let i = 0; i < 20; i++) {
-		let first_name = faker.name.firstName();
+  for (let i = 0; i < 20; i++) {
+    let first_name = faker.name.firstName();
     let last_name = faker.name.lastName();
-		let email = faker.internet.email();
-		let department_id = faker.random.number({min:1,max:5});
+    let email = faker.internet.email();
+    let department_id = faker.random.number({min:1,max:5});
 		
-		employees.push({
-			first_name,
+    employees.push({
+      first_name,
       last_name,
-			email,
-			department_id
-		});
-	}
-	
-	return employees;
+      email,
+      department_id
+      });
+    }
+  return employees;
 }
 
-// Create json files
+
 let employees = generateEmployees();
 let employeeStream = createWriteStream('./data/employees.json');
 employeeStream.write(JSON.stringify(employees));

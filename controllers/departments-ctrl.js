5bfbe52a99fc898724f10cb0b33e63afb5e0f4c7
dhazlettjr <d.hazlett.jr@gmail.com>
2018-03-22 13:34:33 -0500

    added req params for one department

--- controllers/departments-ctrl.js
+++ controllers/departments-ctrl.js
@@ -1,4 +1,4 @@
-const { getAllDepartments } = require('../models/Departments');
+const { getAllDepartments, getOneDepartment } = require('../models/Departments');
 
 module.exports.getDepartments = (req, res, next) => {
   getAllDepartments()
@@ -7,3 +7,17 @@ module.exports.getDepartments = (req, res, next) => {
   })
   .catch (err => next(err));
 };
+
+module.exports.getDepartment = (req, res, next) => {
+  getOneDepartment(req.params.departmentId)
+  .then(dept => {
+    if(dept){
+      res.status(200).json(dept);
+    } else {
+      let error = new Error('Department not found!');
+      error.status = 404;
+      next(error);
+    }
+  })
+  .catch(err => next(err));
+}

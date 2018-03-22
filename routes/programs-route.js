e2f7cf7a83794d923d77306b423999e098767d12
Hunter Phillips <hkphillips42@gmail.com>
2018-03-22 11:50:38 -0500

    variable name

--- routes/programs-route.js
+++ routes/programs-route.js
@@ -3,11 +3,11 @@ const programRouter = Router();
 const { getPrograms, getProgram, addProgram, editProgramByDate } = require('../controllers/programs-ctrl');
 
 programRouter.get("/programs", getPrograms);
+//add new program when a POST is sent to /programs url
+programRouter.post("/programs", addProgram);
 //get program by req.param.programId
 programRouter.get("/programs/:programId", getProgram);
 //edit program identified by id
 programRouter.put("/programs/:programId", editProgramByDate)
-//add new program when a POST is sent to /programs url
-programRouter.post("/programs", addProgram);
 
 module.exports = programRouter;

d7e4f7c54216cfd25f0a3b290adb471f918ac0dc
Valerie Freeman <valerah7@gmail.com>
2018-03-23 12:08:06 -0500

    Created model file for functions to get, post, put products

---
+++ models/Product.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./bangazon.sqlite");
+
+//Get all products
+module.exports.getAllProducts = () => {
+  return new Promise ((reject, resolve) => {
+    db.all(`SELECT * FROM products`, (prods, err) => {
+      if (err) reject(err);
+      resolve(prods);
+    });
+  });
+};
+
+//Get single product
+module.exports.getSingleProduct = (prodID) => {
+  return new Promise ((resolve, reject) => {
+    db.get(`SELECT * FROM products
+    WHERE product_id = ${prodID}`, (err, product) => {
+      if (err) return reject(err);
+      resolve(product);
+    });
+  });
+};
+
+//Add a product
+module.exports.addSingleProduct = ({ title, price, product_type_id, customer_id, description }) => {
+  return new Promise ((resolve, reject) => {
+    db.run(`INSERT INTO products
+    VALUES (null, "${title}", "${price}", ${product_type_id}, ${customer_id}, "${description}")`,
+    function(err, product) {
+      if (err) return reject(err);
+      resolve({id: this.lastID});
+    });
+  });
+};
+
+//Edit a product
+module.exports.editProduct = (id, { column, value }) => {
+  return new Promise((resolve, reject) => {
+      db.run(`UPDATE products SET "${column}" = "${value}"
+      WHERE product_id = ${id}
+      `, function (err) {
+        if (err) { return reject(err) };
+        resolve({changes: this.changes});
+      });
+  });
+};
+
+//TODO: Decide as a team how and when to delete a product
+//Delete a product
+module.exports.deleteProduct = () => {
+  
+};
